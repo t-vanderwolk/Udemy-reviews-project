de11f08280ee91cd096139d04e542a6d7221b149
Taylor Vanderwolk <taylorvanderwolk@Taylors-MBP.lan>
2023-04-10 11:30:56 -0700

    Reviews - React Icons

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,4 +1,14 @@
+
+import { useState } from 'react';
+import people from './data'
+import {FaBeer} from 'react-icons/fa'
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+ const {name, job, image, text}= people[index];
+ console.log(name)
+  return <div>
+    <h2>Reviews Starter</h2>
+    <FaBeer className='beer'/>
+    </div>;
 };
 export default App;
