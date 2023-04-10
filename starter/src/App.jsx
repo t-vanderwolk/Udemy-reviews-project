435785e0522c1cbf7c5c7a3d0170e0ca1742127e
Taylor Vanderwolk <taylorvanderwolk@Taylors-MBP.lan>
2023-04-10 11:54:10 -0700

    Reviews - Prev and Next

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -5,7 +5,23 @@ import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa'
 const App = () => {
   const [index, setIndex] = useState(0);
  const {name, job, image, text}= people[index];
- console.log(name)
+
+
+const nextPerson = ()=> {
+  setIndex((currentIndex)=> {
+    const newIndex = currentIndex + 1;
+    return newIndex;
+  })
+};
+const prevPerson = ()=> {
+  setIndex((currentIndex)=> {
+    const newIndex = currentIndex - 1;
+    return newIndex;
+  })
+};
+
+
+
   return (
   <main>
     <article className='review'>
@@ -18,6 +34,14 @@ const App = () => {
 <h4 className='author'>{name}</h4>
 <p className='job'>{job}</p>
 <p className='info'>{text}</p>
+<div className='btn-container'>
+  <button className='prev-btn' onClick={prevPerson}>
+    <FaChevronLeft />
+  </button>
+  <button className='next-btn' onClick={nextPerson}>
+    <FaChevronRight />
+  </button>
+</div>
     </article>
     </main>
   );
