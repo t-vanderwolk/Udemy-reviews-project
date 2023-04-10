376e4c0dc35629d22199a3f99c09357415cfded1
Taylor Vanderwolk <taylorvanderwolk@Taylors-MBP.lan>
2023-04-10 12:14:04 -0700

    Reviews - Check Number

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -2,28 +2,35 @@
 import { useState } from 'react';
 import people from './data'
 import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa'
-const App = () => {
+const  App = () => {
   const [index, setIndex] = useState(0);
- const {name, job, image, text}= people[index];
+  const { name, job, image, text } = people[index];
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
 
 
-const nextPerson = ()=> {
-  setIndex((currentIndex)=> {
-    const newIndex = currentIndex + 1;
-    return newIndex;
-  })
-};
-const prevPerson = ()=> {
-  setIndex((currentIndex)=> {
-    const newIndex = currentIndex - 1;
-    return newIndex;
-  })
-};
 
-
-
-  return (
-  <main>
+  return(
+     <main>
     <article className='review'>
       <div className='img-container'>
       <img src={image} alt={name} className='person-img' />
@@ -45,5 +52,7 @@ const prevPerson = ()=> {
     </article>
     </main>
   );
-};
+}
+  
+
 export default App;
